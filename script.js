199e98dad49b547543251ae1bb3f55f0453589f9
muslimajahan <mousumi0699@gmail.com>
2024-01-08 18:53:19 -0330

    added javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+var xxcontainer = document.getElementById('xxcontainer');
+xxcontainer.innerHTML = '';
+xxcontainer.id = 'newxxContainer';
+var itemsArray = [
+    { id: 1, name: "Item 1", description: "Description for Item 1" },
+    { id: 2, name: "Item 2", description: "Description for Item 2" },
+    { id: 3, name: "Item 3", description: "Description for Item 3" }
+  ];
+  console.log(itemsArray[1]); 
+  console.log(itemsArray[2]); 
+  console.log(itemsArray[3]); 
